e831431a5cc50947ab064a669d7fdbc93ab0e2ec
olegs7771 <olegs7771@gmail.com>
2020-11-19 23:18:50 +0200

    ok

--- client/src/components/skills/Skills.js
+++ client/src/components/skills/Skills.js
@@ -1,8 +1,89 @@
 import React, { Component } from "react";
+import spriteIcon from "../../../src/img/sprite_brand.svg";
 
 class Skills extends Component {
   render() {
-    return <section className="skills">Skills</section>;
+    return (
+      <section className="skill">
+        <div className="heading-1 heading-1__text--dark mb-md ">My Skills</div>
+        <div className="skill__icon-container">
+          {/* FRONT-END  */}
+          <div className=" skill__icon-box">
+            <h2 className="skill__icon-box--front">Front End</h2>
+            <ul className="skill__list--front">
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-css3"} />
+                </svg>
+                <span className="skill__icon--name">css</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-javascript"} />
+                </svg>
+                <span className="skill__icon--name">JavaScript</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-sass"} />
+                </svg>
+                <span className="skill__icon--name">sass</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-heroku"} />
+                </svg>
+                <span className="skill__icon--name">heroku</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-npm"} />
+                </svg>
+                <span className="skill__icon--name">npm</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-react"} />
+                </svg>
+                <span className="skill__icon--name">React</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-react"} />
+                </svg>
+                <span className="skill__icon--name">React Native</span>
+              </li>
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-redux"} />
+                </svg>
+                <span className="skill__icon--name">Redux</span>
+              </li>
+            </ul>
+          </div>
+
+          {/* BACK-END  */}
+          <div className=" skill__icon-box">
+            <h2 className="skill__icon-box--back">Back End</h2>
+            <ul className="skill__list--back">
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-node-dot-js"} />
+                </svg>
+                <span className="skill__icon--name">node.js</span>
+              </li>
+
+              <li className="skill__item">
+                <svg className="skill__icon">
+                  <use href={spriteIcon + "#icon-mongodb"} />
+                </svg>
+                <span className="skill__icon--name">Mongo</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 export default Skills;
