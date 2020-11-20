b2de9ec9e21cf347c96c37709e30af1aced968ee
olegs7771 <olegs7771@gmail.com>
2020-11-20 23:46:36 +0200

    ok

--- client/src/components/contact_form/Contact.js
+++ client/src/components/contact_form/Contact.js
@@ -2,7 +2,55 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-    return <section className="contact">Contact Form</section>;
+    return (
+      <section className="contact">
+        <div className="heading-1 heading-1__text--light mb-lg text-upcase ">
+          Contact Me
+        </div>
+        <div className="contact__form-container">
+          <form action="" className="contact__form">
+            <span className="contact__form-label">Name</span>
+            <input
+              type="text"
+              name="name"
+              className="contact__form-input--name"
+            />
+            <span className="contact__form-label">Email</span>
+            <input
+              type="email"
+              name="email"
+              className="contact__form-input--email"
+            />
+            <input
+              type="submit"
+              value="Send"
+              className="contact__form-input--submit"
+            />
+          </form>
+          <div className="contact__block">
+            <p className="contact__block-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              sapiente deleniti saepe perferendis quisquam non laborum tempora
+              est aspernatur minus.
+            </p>
+            <ul className="contact__block-list">
+              <li className="contact__block-item">
+                <span className="contact__block-icon">Icon</span>
+                <span className="contact__block-phone">0503054422</span>
+              </li>
+              <li className="contact__block-item">
+                <span className="contact__block-icon">Icon</span>
+                <span className="contact__block-phone">Email</span>
+              </li>
+              <li className="contact__block-item">
+                <span className="contact__block-icon">Icon</span>
+                <span className="contact__block-phone">facebook</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 export default Contact;
--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -4,16 +4,43 @@ class Projects extends Component {
   render() {
     return (
       <section className="project">
-        <div className="heading-1 heading-1__text--light mb-md text-upcase ">
+        <div className="heading-1 heading-1__text--light mb-lg text-upcase ">
           My Projects
         </div>
-        <div className="project__content">
-          <ul className="project__list">
-            <li className="project__item">Project</li>
-            <li className="project__item">Project</li>
-            <li className="project__item">Project</li>
-          </ul>
-        </div>
+
+        <ul className="project__list">
+          <li className="project__item">
+            <div className="project__card-title mb-sm">Project Title</div>
+            <p className="project__card-article">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+              asperiores numquam nostrum commodi facilis sed ratione! Nobis
+              minima, voluptates impedit quibusdam blanditiis doloribus unde,
+              voluptatum ad aut, tempore a ratione?
+            </p>
+          </li>
+          <li className="project__item">
+            <div className="project__card">
+              <div className="project__card-title mb-sm">Project Title</div>
+              <p className="project__card-article">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+                asperiores numquam nostrum commodi facilis sed ratione! Nobis
+                minima, voluptates impedit quibusdam blanditiis doloribus unde,
+                voluptatum ad aut, tempore a ratione?
+              </p>
+            </div>
+          </li>
+          <li className="project__item">
+            <div className="project__card">
+              <div className="project__card-title mb-sm">Project Title</div>
+              <p className="project__card-article">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+                asperiores numquam nostrum commodi facilis sed ratione! Nobis
+                minima, voluptates impedit quibusdam blanditiis doloribus unde,
+                voluptatum ad aut, tempore a ratione?
+              </p>
+            </div>
+          </li>
+        </ul>
       </section>
     );
   }
