1c37e212653cc98ba893804d4dcd3aeb2c37aab9
olegs7771 <olegs7771@gmail.com>
2021-01-04 20:25:22 +0200

    ok

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const contact = require('./routes/contact');
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // BODY PARSER WITH LIMITTED BODY
 app.use(express.json({ limit: '10kb' }));
