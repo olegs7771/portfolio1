b83d22130fee9476cb7a4e6c6fa447b072cacfbb
olegs7771 <olegs7771@gmail.com>
2021-01-04 12:25:37 +0200

    ok

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const app = require('./app');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 
 // require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT || 5000, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+  },
+  app
+);
+
+sslServer.listen(PORT || 5000, () =>
+  console.log(`Secure server 🔑 on port ${PORT}`)
+);
+
 //
