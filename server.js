99278597e6f0658c9e15a0a91837e60346b00381
olegs7771 <olegs7771@gmail.com>
2021-01-04 19:42:41 +0200

    ok

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
+console.log('process.env', process.env.PORT);
 
-app.listen(PORT || 5000, () => console.log(`Secure server 🔑 on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Secure server 🔑 on port ${PORT}`)
+);
