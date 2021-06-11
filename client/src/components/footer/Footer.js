ad8caf28e9a845cf45f0a64d19cd309f01600db1
olegs7771 <olegs7771@gmail.com>
2021-06-10 21:26:59 -0500

    ok

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+    const CurrentDate = () => new Date().getFullYear();
+
     return (
       <footer className={this.props.isBlured ? 'footer is_blured' : 'footer'}>
-        <p className="footer__copy">&copy; 2020 All rights reserved</p>
+        <p className="footer__copy">
+          &copy; <CurrentDate /> All rights reserved
+        </p>
       </footer>
     );
   }
